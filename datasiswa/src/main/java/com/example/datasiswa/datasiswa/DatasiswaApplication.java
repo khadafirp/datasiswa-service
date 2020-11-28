b4c4e84a98a6a9ef98e4e66e99215a2d68347502
khadafi <khadafi@kali.kali>
2020-11-28 10:07:53 -0500

    first commit

---
+++ datasiswa/src/main/java/com/example/datasiswa/datasiswa/DatasiswaApplication.java
@@ -0,0 +1,13 @@
+package com.example.datasiswa.datasiswa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasiswaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasiswaApplication.class, args);
+	}
+
+}
---
+++ datasiswa/src/test/java/com/example/datasiswa/datasiswa/DatasiswaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datasiswa.datasiswa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasiswaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
